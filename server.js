f5fd185f63066332f5fb10cceb19a80c882efab1
Jeff Hoefs <jeff.hoefs@gmail.com>
2016-01-18 17:00:28 -0800

    cleanup

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ process.on('exit', function() {
 /**************** COMMAND LINE OPTIONS *************/
 var program = require('commander');
 program
-  .version('0.3.0')
+  .version('0.4.0')
   .option('-p, --port <device>', 'Specify the serial port [/dev/tty.usbmodemfd121]', '/dev/tty.usbmodemfd121')
   .option('-s, --server <port>', 'Specify the port [8887]', Number, 8887)
   .option('-m, --multi <connection>', 'Enable multiple connections [false]', "false")
@@ -76,7 +76,6 @@ serial.on("error", function(msg) {
   process.exit(1);
 });
 
-
 /************************* SOCKET.IO ***********************/
 
 socketIO.sockets.on('connection', function(socket) {
@@ -111,11 +110,6 @@ socketIO.sockets.on('connection', function(socket) {
 
     debug("disconnected " + socket.id);
     connectedClients--;
-
-    // TODO - figure out how to get this to work
-    //var clientList = socketIO.sockets.adapter.rooms['/'];
-    //var clientList = Object.keys(socketIO.sockets.adapter.rooms[room]).length = 0;
-    //var numRemaining = clientList.length;
 
     if (!enableMultiConnect || connectedClients < 1) {
       connectedSocket = null;
